7ee7e745958510543e5711dbf44894fac9970f59
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-05-22 22:45:06 +0530

    Manage vehicle class

---
+++ Java-Serialization/src/Vehicle.java
@@ -0,0 +1,15 @@
+public class Vehicle {
+    private String color;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Vehicle(String color) {
+        this.color = color;
+    }
+}
